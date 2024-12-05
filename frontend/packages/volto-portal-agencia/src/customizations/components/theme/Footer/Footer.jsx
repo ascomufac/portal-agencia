90551012ee70b8d9323424946cea92fa65beaaac
Veridiano Barroso <veridiano.barroso@instivo.com.br>
2024-12-05 11:22:39 -0500

    add css in component

--- frontend/packages/volto-portal-agencia/src/customizations/components/theme/Footer/Footer.jsx
+++ frontend/packages/volto-portal-agencia/src/customizations/components/theme/Footer/Footer.jsx
@@ -1,9 +1,21 @@
-import style from './Footer.module.css';
+import React from 'react';
 import LogoRodape from './logo-ifes-branca.svg';
 
 export default function Footer() {
+  const styles = {
+    footerContainer: {
+      height: '30%',
+      padding: '8rem',
+      background: '#022336',
+      color: 'aliceblue',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  };
+
   return (
-    <footer className={style.footerContainer}>
+    <footer style={styles.footerContainer}>
       <img src={LogoRodape} alt="Agência Ifes" />
     </footer>
   );
--- frontend/packages/volto-portal-agencia/src/customizations/components/theme/Header/Header.jsx
+++ frontend/packages/volto-portal-agencia/src/customizations/components/theme/Header/Header.jsx
@@ -1,5 +1,5 @@
 import { Anontools, Logo, Navigation } from '@plone/volto/components';
-import style from './Header.module.css';
+import React from 'react';
 
 export default function Header() {
   const getFormattedDate = () => {
@@ -12,22 +12,63 @@ export default function Header() {
     return new Date().toLocaleDateString('pt-BR', options);
   };
 
+  const styles = {
+    containerHeaderLogo: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      margin: 'auto',
+      padding: '1rem 10rem',
+      backgroundColor: '#cee1ec',
+    },
+    navegationHeader: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      margin: 'auto',
+      backgroundColor: '#c1d1da',
+    },
+    navTopContainer: {
+      color: '#e1e5e7',
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      gap: '2rem',
+      paddingTop: '2rem',
+      backgroundColor: '#022336',
+      padding: '2rem',
+      paddingLeft: '2rem',
+    },
+    navTopContainerLinks: {
+      display: 'flex',
+      gap: '2rem',
+    },
+    navLink: {
+      color: 'aliceblue',
+      textDecoration: 'none',
+    },
+  };
+
   return (
     <div>
-      <nav className={style.navTopContainer}>
-        <div className={style.navTopContainerLinks}>
-          <a href="http://">Sala de Imprensa</a>
-          <a href="http://">Área de Pesquisadores</a>
+      <nav style={styles.navTopContainer}>
+        <div style={styles.navTopContainerLinks}>
+          <a href="http://" style={styles.navLink}>
+            Sala de Imprensa
+          </a>
+          <a href="http://" style={styles.navLink}>
+            Área de Pesquisadores
+          </a>
         </div>
       </nav>
-      <div className={style.containerHeaderLogo}>
+      <div style={styles.containerHeaderLogo}>
         <div>{getFormattedDate()}</div>
         <Logo />
         <div>
           <Anontools />
         </div>
       </div>
-      <div className={style.navegationHeader}>
+      <div style={styles.navegationHeader}>
         <Navigation />
       </div>
     </div>
